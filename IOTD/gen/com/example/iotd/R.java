2765d727bf62768655c8f69ea434b352d5fef840
Aluno <Aluno@10.0.0.150>
2014-04-05 09:02:13 -0300

    Terceira aula

--- IOTD/gen/com/example/iotd/R.java
+++ IOTD/gen/com/example/iotd/R.java
@@ -25,11 +25,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int image_of_the_day=0x7f020001;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_action_share=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int image_of_the_day=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f090004;
+        public static final int action_refresh=0x7f090004;
+        public static final int action_share=0x7f090005;
         public static final int data=0x7f090001;
         public static final int descricao=0x7f090003;
         public static final int imagem=0x7f090002;
@@ -42,10 +45,11 @@ public final class R {
         public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
+        public static final int action_refresh=0x7f060001;
+        public static final int action_share=0x7f060002;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
-        public static final int loading=0x7f060003;
+        public static final int hello_world=0x7f060003;
+        public static final int loading=0x7f060004;
     }
     public static final class style {
         /** 
--- IOTD/src/com/example/iotd/MainActivity.java
+++ IOTD/src/com/example/iotd/MainActivity.java
@@ -1,53 +1,80 @@
 package com.example.iotd;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        displayData(null);
-        RssService service = new RssService(this);
-        service.execute(new RssParser()); 
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		refresh();
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void displayData(RssItem item) {
-    	TextView titulo = (TextView)
-    			findViewById(R.id.titulo);
-    	TextView data = (TextView)
-    			findViewById(R.id.data);
-    	TextView descricao = (TextView)
-    			findViewById(R.id.descricao);
-    	ImageView imagem = (ImageView)
-    			findViewById(R.id.imagem);
-    	
-    	if (item != null) {
-    		titulo.setText(item.getTitulo());
-    		data.setText(item.getDate());
-    		descricao.setText(item.getDescricao());
-    		imagem.setImageBitmap(item.getImagem());
-    	} else {
-    		titulo.setText(null);
-    		data.setText(null);
-    		descricao.setText(null);  
-    		imagem.setImageBitmap(null);
-    	}
-    	
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void displayData(RssItem item) {
+		TextView titulo = (TextView) findViewById(R.id.titulo);
+		TextView data = (TextView) findViewById(R.id.data);
+		TextView descricao = (TextView) findViewById(R.id.descricao);
+		ImageView imagem = (ImageView) findViewById(R.id.imagem);
+
+		if (item != null) {
+			titulo.setText(item.getTitulo());
+			data.setText(item.getDate());
+			descricao.setText(item.getDescricao());
+			imagem.setImageBitmap(item.getImagem());
+		} else {
+			titulo.setText(null);
+			data.setText(null);
+			descricao.setText(null);
+			imagem.setImageBitmap(null);
+		}
+
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_refresh:
+			refresh();
+			return true;
+		case R.id.action_share:
+			share();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+	
+	public void refresh() {	
+		displayData(null);
+		RssService service = new RssService(this);
+		service.execute(new RssParser());
+	}
+	
+	public void share() {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT,
+				getText(R.string.app_name)
+				+": "+"titulo - link");
+		startActivity(Intent.
+				createChooser(intent,
+						getText(R.string.action_share)));
+		finish();
+	}
+
 }
